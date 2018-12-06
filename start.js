25c6630c3dd0169854c6fb0737e63f27829fc00b
tom_ptrs@yahoo.com <tom_ptrs@yahoo.com>
2018-12-06 16:35:05 +0000

    fix

--- start.js
+++ start.js
@@ -1,10 +1,23 @@
-var express = require("express");
 
-var app = express();
+
+
+app.listen(8080);
+
+'use strict';
+
+// [START gae_node_request_example]
+const express = require('express');
+
+const app = express();
 
 app.get("/",function(req,res){
 	res.send(200,"Hallo Tom");
 });
 
-
-app.listen(8080);
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END gae_node_request_example]
